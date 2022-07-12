dc962f16030383048bc18ff628487dfa91d2962b
Gargoros <vargveresk@yandex.ru>
2022-07-12 21:33:24 +0300

    added Scaffold block in main class

--- flutter/first_app/17/flutter_complete_guide/lib/main.dart
+++ flutter/first_app/17/flutter_complete_guide/lib/main.dart
@@ -6,7 +6,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app!'),
+        ),
+        body: Text('This is default text!'),
+      ),
     );
   }
 }
