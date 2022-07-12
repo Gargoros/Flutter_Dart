86e7d4cd78fecaf935e09ef896b8217bd6d5a646
Gargoros <vargveresk@yandex.ru>
2022-07-12 22:20:50 +0300

    added column and button in main scaffold block

--- flutter/first_app/17/flutter_complete_guide/lib/main.dart
+++ flutter/first_app/17/flutter_complete_guide/lib/main.dart
@@ -5,12 +5,44 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "Whaat's your favorite color?",
+      "What's your favorite animal?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app!'),
         ),
-        body: Text('This is default text!'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 1'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.green,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 2'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.green,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 3'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.green,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
