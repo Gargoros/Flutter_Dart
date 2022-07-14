8a67f55180bfd2a1941d6194f43eafd1de4a650d
Gargoros <vargveresk@yandex.ru>
2022-07-14 19:15:52 +0300

    intigrate variable _totalScore

--- flutter/first_app/17/flutter_complete_guide/lib/main.dart
+++ flutter/first_app/17/flutter_complete_guide/lib/main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
     {
       'questionText': "Whaat's your favorite color?",
       'answer': [
-        {'text': 'Red', 'score': 10},
+        {'text': 'Red', 'score': 1},
         {'text': 'Blue', 'score': 5},
         {'text': 'Green', 'score': 4},
         {'text': 'White', 'score': 2}
@@ -26,7 +26,7 @@ class _MyAppState extends State<MyApp> {
     {
       'questionText': "What's your favorite animal?",
       'answer': [
-        {'text': 'Puma', 'score': 7},
+        {'text': 'Puma', 'score': 1},
         {'text': 'Cat', 'score': 12},
         {'text': 'Dog', 'score': 23},
         {'text': 'Rabbit', 'score': 8}
@@ -44,13 +44,10 @@ class _MyAppState extends State<MyApp> {
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
-    if (_questionIndex < _questions.length) {
-      print('We have more questions!');
-    } else {
-      print('No more questions!');
-    }
+  void _answerQuestion(int score) {
+    _totalScore += score;
 
     setState(() {
       _questionIndex += 1;
@@ -70,7 +67,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
