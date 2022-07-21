84c33fbff32af134c0e5aef58b4173771c856a25
Gargoros <vargveresk@yandex.ru>
2022-07-21 23:04:04 +0300

    refactor main file, converter to another extends

--- flutter/first_app/home_tasks/first_homework_app/lib/main.dart
+++ flutter/first_app/home_tasks/first_homework_app/lib/main.dart
@@ -5,12 +5,7 @@ void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
